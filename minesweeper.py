3bdb7548b16951df2d555f257c796b42726e17d3
Shweta Mehta <smehta2111@gmail.com>
2019-09-24 22:40:30 +1000

    added comments and fixed a few visuals

--- minesweeper.py
+++ minesweeper.py
@@ -3,9 +3,8 @@ from time import sleep
 
 window = tkinter.Tk()
 
-window.resizable(0, 0)
-
 window.title("Minesweeper")
+window.iconbitmap("MinesweeperAssignment/images/Flag.ico")
 
 #prepare default values
 gameover = False
@@ -36,11 +35,11 @@ customsizes = []
 #Graphics to fill into the cells
 colors = ['#FFFFFF', '#0000FF', '#008200', '#FF0000', '#000084', '#840000', '#008284', '#840084', '#000000']
 mine = tkinter.PhotoImage(file = "MinesweeperAssignment/images/tile_mine.gif")
-flag = tkinter.PhotoImage(file = "MinesweeperAssignment/images/tile_flag.gif")
+flag = tkinter.PhotoImage(file = "MinesweeperAssignment/images/tile_flag.png")
 tile_no = [" ", "❶", "❷", "❸", "❹", "❺", "❻", "❼", "❽"]
 
 
-
+#function for timer label
 def timer():
     global count, firstClick, gameover
     while firstClick == False and gameover == False:
@@ -48,13 +47,15 @@ def timer():
         time.set(count)
         sleep(1)
         timer()
-    restartLabel.set(restartDefault if gameover == False else restartLost)
 
-
+#threading variable
 timeVar = threading.Timer(1.0, timer)
 
+
+#This function creates the menu bar which includes:
 def createMenu():
     menubar = tkinter.Menu(window)
+    # settings to change board size, and
     menusize = tkinter.Menu(window, tearoff=0)
     menusize.add_command(label="Beginner (10x10 with 10 mines)", command=lambda: setSize(10, 10, 10))
     menusize.add_command(label="Intermediate (20x20 with 40 mines)", command=lambda: setSize(20, 20, 40))
@@ -64,15 +65,18 @@ def createMenu():
     for x in range(0, len(customsizes)):
         menusize.add_command(label=str(customsizes[x][0])+"x"+str(customsizes[x][1])+" with "+str(customsizes[x][2])+" mines", command=lambda customsizes=customsizes: setSize(customsizes[x][0], customsizes[x][1], customsizes[x][2]))
     menubar.add_cascade(label="size", menu=menusize)
+    # a close window option
     menubar.add_command(label="exit", command=lambda: window.destroy())
     window.config(menu=menubar)
 
 
+#this function allows user to enter desired board dimensions and number of mines
 def setCustomSize():
     global customsizes
     r = tkinter.simpledialog.askinteger("Custom size", "Enter amount of rows")
     c = tkinter.simpledialog.askinteger("Custom size", "Enter amount of columns")
     m = tkinter.simpledialog.askinteger("Custom size", "Enter amount of mines")
+    #number of mines need to be proportionate to board dimensions
     while m > r*c:
         m = tkinter.simpledialog.askinteger("Custom size", "Maximum mines for this dimension is: " + str(r*c) + "\nEnter amount of mines")
     customsizes.insert(0, (r,c,m))
@@ -80,6 +84,8 @@ def setCustomSize():
     setSize(r,c,m)
     createMenu()
 
+
+#sets values for rows. columns, and mines for system to remember
 def setSize(r,c,m):
     global rows, cols, mines
     rows = r
@@ -88,6 +94,8 @@ def setSize(r,c,m):
     saveConfig()
     restartGame()
 
+
+#saves any customizations made to game as options for future
 def saveConfig():
     global rows, cols, mines
     #configuration
@@ -106,6 +114,9 @@ def saveConfig():
     with open("config.ini", "w") as file:
         config.write(file)
 
+
+#when game is being reloaded, loads last played board dimensions and mines
+#as well as any previous board customisations
 def loadConfig():
     global rows, cols, mines, customsizes
     config = configparser.SafeConfigParser()
@@ -117,6 +128,8 @@ def loadConfig():
     for x in range(0, amountofsizes):
         customsizes.append((config.getint("sizes", "row"+str(x)), config.getint("sizes", "cols"+str(x)), config.getint("sizes", "mines"+str(x))))
 
+
+#spawns mines and makes sure that mines don't spawn on top of each other
 def prepareGame():
     global rows, cols, mines, board
     board = []
@@ -155,14 +168,17 @@ def prepareGame():
                     board[x+1][y-1] = int(board[x+1][y-1]) + 1
             if board[x+1][y] != -1:
                 board[x+1][y] = int(board[x+1][y]) + 1
-            if y != cols-1:
+            if y != cols-1:                                                                                                                                                                                        
                 if board[x+1][y+1] != -1:
                     board[x+1][y+1] = int(board[x+1][y+1]) + 1
 
+
+#setup for board display
 def prepareWindow():
     global rows, cols, buttons, time
-    tkinter.Button(window, textvariable=restartLabel, width = 2, command=restartGame).grid(row=0, column=0, columnspan=int(cols/2), sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
-    tkinter.Label(window, textvariable=time, ).grid(row = 0, column = cols - (int(cols/2)), columnspan=int(cols/3), sticky = tkinter.N+tkinter.E+tkinter.S+tkinter.W )
+    restartButton = tkinter.Button(window, textvariable=restartLabel, width = 2, command=restartGame).grid(row=0, column=0, columnspan=int(cols/2), sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
+    timerLabel= tkinter.Label(window, textvariable=time, font=("Times", 10, "bold"))
+    timerLabel.grid(row = 0, column = cols - (int(cols/2)), columnspan=int(cols/2), sticky = tkinter.N+tkinter.E+tkinter.S+tkinter.W)
     buttons = []
     for x in range(0, rows):
         buttons.append([])
@@ -172,6 +188,8 @@ def prepareWindow():
             b.grid(row=x+1, column=y, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
             buttons[x].append(b)
 
+
+#when the restart button is clicked, resets vital game values
 def restartGame():
     global gameover, firstClick, count, timeVar, time
     timeVar.cancel()
@@ -189,8 +207,10 @@ def restartGame():
     timeVar = threading.Timer(1.0, timer)
     prepareWindow()
 
+
+#this function executes when a button(cell) is clicked on
 def clickOn(x,y):
-    global board, buttons, colors, gameover, rows, cols, tile_no, time, firstClick, timeVar, colors
+    global board, buttons, colors, gameover, rows, cols, tile_no, time, firstClick, timeVar, colors, restartLost
     if firstClick:
         prepareGame()
         firstClick = False
@@ -199,16 +219,17 @@ def clickOn(x,y):
         return
     buttons[x][y].config(text = str(board[x][y]))
     if board[x][y] == -1:
-        buttons[x][y].grid(sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
-        buttons[x][y].config(image = mine, text = "*")
-        buttons[x][y].config(background='red')
+        buttons[x][y].config(image = '')
+        buttons[x][y].config(image = mine, text = "*", background='red')
         gameover = True
         #now show all other mines
         for _x in range(0, rows):
             for _y in range(cols):
                 if board[_x][_y] == -1:
                     buttons[x][y]['state'] = 'disabled'
+                    buttons[_x][_y].config(image = '')
                     buttons[_x][_y].config(image = mine, text = "*")
+        restartLabel.set(restartLost)
         tkinter.messagebox.showinfo("Game Over", "You have lost.")
     else:
         buttons[x][y].config(disabledforeground= colors[board[x][y]], text = tile_no[board[x][y]])
@@ -220,6 +241,8 @@ def clickOn(x,y):
     buttons[x][y].config(relief=tkinter.SUNKEN)
     checkWin()
 
+
+#function that has been set up to reveal pockets of cells that have no mines around
 def autoClickOn(x,y):
     global board, buttons, colors, rows, cols, tile_no
     if buttons[x][y]["state"] == "disabled":
@@ -249,6 +272,8 @@ def autoClickOn(x,y):
         if x != rows-1 and y != cols-1:
             autoClickOn(x+1,y+1)
 
+
+#function that toggles between displaying a flag and "unflagging" when a cell is right clicked 
 def onRightClick(x,y):
     global buttons, flag
     if gameover:
@@ -257,13 +282,15 @@ def onRightClick(x,y):
         buttons[x][y].config(image = '', text = " ")
         buttons[x][y]["state"] = "normal"
     elif buttons[x][y]["text"] == " " and buttons[x][y]["state"] == "normal":
-        for i in range(0, 2): 
+        #buttons[x][y]["state"] = "disabled"
+        for i in range(0, 2):
             buttons[x][y].config(image = flag, text = "?")
             buttons.image = flag
-        buttons[x][y]["state"] = "disabled"
-
+        
+        
+#function checkWin is called everytime a cell is clicked on to check if win conditions have been met
 def checkWin():
-    global buttons, board, rows, cols, gameover, timeVar, time, firstClick
+    global buttons, board, rows, cols, gameover, timeVar, time, firstClick, restartWon
     win = True
     for x in range(0, rows):
         for y in range(0, cols):
@@ -271,21 +298,22 @@ def checkWin():
                 win = False
     if win:
         firstClick = True
-        restartLabel.set(restartWon)
         gameover = True
         for _x in range(0, rows):
             for _y in range(cols):
                 if board[_x][_y] == -1:
                     buttons[x][y]['state'] = 'disabled'
                     buttons[_x][_y].config(image = mine, text = "*")
+        restartLabel.set(restartWon)
         tkinter.messagebox.showinfo("Gave Over", "You have won.")
 
+#loading configuration file for game 
 if os.path.exists("config.ini"):
     loadConfig()
-else:
+else:#saving config to file config.ini if one doesn't already exist
     saveConfig()
 
+#main game area that sets up th game and then loops the window to reflect any changes
 createMenu()
-
 prepareWindow()
 window.mainloop()
